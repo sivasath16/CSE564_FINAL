5188b3d94b6183821126a0edece3cdf8f2dc6b44
sivasath16 <75243395+sivasath16@users.noreply.github.com>
2024-04-24 19:50:21 -0400

    JS, CSS files and file structure

---
+++ static/dashboard/geo.js
@@ -0,0 +1,7 @@
+const margin = { top: 30, right: 10, bottom: 60, left: 150 };
+const width = 1000 - margin.left - margin.right;
+const height = 700 - margin.top - margin.bottom
+
+function geoMap(){
+    
+}
