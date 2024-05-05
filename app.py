43af0ac17b774fc03d56f6d39f9ff8f077dc5e5a
sivasath16 <75243395+sivasath16@users.noreply.github.com>
2024-05-05 13:56:09 -0400

    changed number of clusters and removed Entity from PCP.

--- app.py
+++ app.py
@@ -9,30 +9,33 @@ app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
 # Load the cancer dataset
-cancer_data = pd.read_csv('./dataset/dataset.csv')
+cancer_data = pd.read_csv('./static/dataset/dataset.csv')
+cancer_data = cancer_data.sample(frac=0.002, random_state=42)
 pcp_df = cancer_data.copy()
+exclude1 = ['Entity', 'Code']
+pcp_df.drop(exclude1, inplace=True, axis=1)
 numerical_columns = cancer_data.select_dtypes(include=[np.number])
-exclude = ["Year"]
+exclude = ["Year", "m49_code"]
 numerical_columns.drop(exclude, inplace=True, axis=1)
 col_names = list(pcp_df.columns)
 pcpCols = col_names.copy()
-# print(pcpCols)
-# print(numerical_columns.columns)
 
 @app.route('/')
 def index():
     return render_template('dashboard.html')
 
-@app.route('/cancer_data', methods=['GET'])
-def get_cancer_data():
-    # Convert the DataFrame to a JSON object
-    cancer_data_json = cancer_data.to_json(orient='records')
-    return jsonify(cancer_data_json)
+# @app.route('/cancer_data', methods=['GET'])
+# def get_cancer_data():
+#     # Convert the DataFrame to a JSON object
+#     cancer_data_json = cancer_data.to_json(orient='records')
+#     return jsonify(cancer_data_json)
 
 
 @app.route('/pcp', methods=['GET'])
 def pcp():
-    num_clusters1 = 4
+    num_clusters1 = 8
+    
+    print(num_clusters1)
 
     kmeans = KMeans(n_clusters=num_clusters1, random_state=42)
     kmeans.fit(numerical_columns)
